294cc63d3e367d3f9a489bc986b909d25c33dfc2
Asipka <asipomars@gmail.com>
2020-10-29 15:11:39 +0300

    Косметика интерфейса: убрал возможность скалирования интерйефса. Всегда одинаковый размер.

--- MainWindow.py
+++ MainWindow.py
@@ -14,7 +14,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
         MainWindow.resize(641, 453)
+        MainWindow.setMinimumSize(QtCore.QSize(641, 453))
+        MainWindow.setMaximumSize(QtCore.QSize(641, 453))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
